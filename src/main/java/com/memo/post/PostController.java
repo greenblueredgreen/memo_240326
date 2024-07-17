6260e92d55941e237c44788a6e1319944d7c81f4
ahyun <kaeruruk@naver.com>
2024-07-17 14:22:46 +0900

    memo 글목록 list 결석강의

--- src/main/java/com/memo/post/PostController.java
+++ src/main/java/com/memo/post/PostController.java
@@ -24,10 +24,10 @@ public class PostController {
 	public String postListView(HttpSession session, Model model) {
 		//로그인 여부 확인
 		Integer userId = (Integer)session.getAttribute("userId");  //int는 null 저장 불가, Integer은 null 가능
-//		if(userId == null) {
-//			// 비로그인 상태 : null => 로그인 페이지로 redirect 이동
-//			return "redirect:/user/sign-in-view";  //메소드종료
-//		}
+		if(userId == null) {
+		// 비로그인 상태 : null => 로그인 페이지로 redirect 이동
+		return "redirect:/user/sign-in-view";  //메소드종료
+		}
 		
 		//db조회 - 글목록
 		List<Post> postList = postBO.getPostListByUserId(userId);
@@ -35,7 +35,6 @@ public class PostController {
 		// model 에 담기 (controller 하고 view는 끊어져 있기 때문에 model 이 연결해준다.)
 		// 화면 같은 html 에서만 model이 존재
 		model.addAttribute("postList", postList);
-		
 		
 		return "post/postList";
 	}
